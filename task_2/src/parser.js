40ea89330ef845cf482c9fd854fe7d7073289dff
George Ognevoy <gognevoy@cushing.software>
2018-04-23 16:19:00 +0300

    parser

--- task_2/src/parser.js
+++ task_2/src/parser.js
@@ -4,14 +4,13 @@ const levenshtein = require('fast-levenshtein');
 
 const limitOfLevenstein = 1;
 
-const finder = ({inputArray, patternArray}, handler) => {
-  return inputArray.filter(inputString => {
-    return patternArray.find(patternString => {
-      return handler({inputString, patternString});
-    });
-  });
-};
-
+const finder = ({inputArray, patternArray}, handler) =>
+  inputArray.filter(inputString =>
+    patternArray.find(patternString =>
+      handler({inputString, patternString})
+    )
+  );
+  
 const mode1 = ({inputArray, patternArray}) => finder({inputArray, patternArray}, ({inputString,patternString})=> inputString.trim() === patternString.trim());
 const mode2 = ({inputArray, patternArray}) => finder({inputArray, patternArray}, ({inputString,patternString})=> inputString.trim().includes(patternString.trim()));
 const mode3 = ({inputArray, patternArray}) => finder({inputArray, patternArray}, ({inputString,patternString})=> levenshtein.get(inputString.trim(), patternString.trim()) <= limitOfLevenstein);
